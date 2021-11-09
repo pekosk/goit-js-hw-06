0fe1a23c03f8a3eb5ab109117b98947c683592be
pekosk <rkostyk64@gmail.com>
2021-11-09 16:22:37 +0200

    2 task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsList = document.querySelector('#ingredients');
+
+const ingredientsArr = ingredients.map(ingred => {
+const item = document.createElement('li');
+item.textContent = ingred;
+item.classList.add('item');
+return item;
+})
+
+ingredientsList.append(...ingredientsArr);
